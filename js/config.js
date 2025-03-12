2263b69ac828f2c32fcb233967fd38d6ad9b21bf
Jaydev Patel <50128503+ijaydevpatel@users.noreply.github.com>
2025-03-12 17:16:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_vS77e8m3L', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '23pbut07fgj7fkf16kr8t9q217', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_iLaqDFfHx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3cdi3929cpcvailnhtmvhm18v3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://y8n1f0hfel.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
