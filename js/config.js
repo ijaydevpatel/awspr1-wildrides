4ce6a1e06eebd82020e2031c1072ce3c583d8bf2
Jaydev Patel <50128503+ijaydevpatel@users.noreply.github.com>
2025-03-12 17:27:57 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_8rWevi6Vm', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4k0c42qfcdgdiqnv511p7c1gcv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_y3kU7BDUm', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2la6epdjc9famlj0sidhntlghj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
